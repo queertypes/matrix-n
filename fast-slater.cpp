4794c84b61b25192bc9d58633b94221cd25d1105
Alejandro Cabrera <cpp.cabrera@gmail.com>
2011-11-19 05:26:27 -0500

    Initial commit.

---
+++ fast-slater.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <sstream>
+using namespace std;
+
+void parse_args(const int argc, const char *const *const argv,
+                size_t &n, size_t &m)
+{
+  if (argc != 3) {
+     fprintf(stderr, "usage: %s <num_electrons> <num_steps>\n", argv[0]);
+     return;
+  }
+
+  if (sscanf(argv[1], "%u", &n) != 1) {
+    fprintf(stderr, "error: converting arg %s to integer failed.\n", argv[1]);
+    return;
+  }
+
+  if (sscanf(argv[2], "%u", &m) != 1) {
+    fprintf(stderr, "error: converting arg %s to integer failed.\n", argv[2]);
+    return;
+  }
+}
+
+
+int main(int argc, char **argv)
+{
+  size_t n = 0 , m = 0;
+
+  parse_args(argc, argv, n, m);
+
+  if ( (n == 0) || (m == 0) ) {
+    fprintf(stderr, "error: parsing arguments failed. Aborting.\n");
+    return 1;
+  }
+
+  printf("Running simultaion with %u electrons for %u steps\n", n, m);
+
+  return 0;
+}
+
