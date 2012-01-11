dfe41a1d5f7f6ddc9c165769dd5420f4f82d7ac1
Alejandro Cabrera <cpp.cabrera@gmail.com>
2012-01-10 20:13:55 -0500

    Preliminary version 2 of NK_slater implemented. Needs testing.

--- octave/nukala_kent_slater_determinant.m
+++ octave/nukala_kent_slater_determinant.m
@@ -1,32 +1,26 @@
 function determinant = nukala_kent_slater_determinant(D, V, num_steps)
 	
-	printf("Start\n");
-
 	# Initialization
 	num_electrons = rows(D);
 	p = 1;
-	step = 1;
-	gamma = zeros();
-	U = zeros(num_steps, num_electrons);
-
-	# Iterative refinement stage
+	gamma = zeros(1, num_steps);
+	U = zeros(num_electrons, num_steps);
+	
+	# Configuration stepping stage
 	for step = 1:num_steps
-		printf("Here: step(%d)\n", step);
 		p = mod(p, num_electrons) + 1;
-		U(step,:) = D(:,p);
+		U(:,step) = D(:,p);
 		for i = 1:step-1
-			U(step,:) = U(step,:) - gamma(1,i) * (V(i,:) * U(step,:)) * U(i,:);
+			U(:,step) = U(:,step) - gamma(1,i) * (V(i,:) * U(:,step)) * U(:,step);
 		endfor
-		R = 1 + V(step,:) * U(step,:);
+		R = 1 + V(step,:) * U(:,step);
 		gamma(1,step) = 1/R;
-		k = k + 1;
 	endfor
-	D_after = 0;
 
 	# Final reduction stage
 	prod = eye(num_electrons, num_electrons);
-	for i = 0:num_steps
-		prod = prod * (eye(num_electrons, num_electrons) - gamma(1,i) * U(,:) * V(i,:));
+	for i = 1:num_steps
+		prod = prod * (eye(num_electrons, num_electrons) - gamma(1,i) * U(:,i) * V(i,:));
 	endfor
 	D_after = prod * D;
 
