8923b95182d6be900f1a3e936a5e4e710aa3605d
Alejandro Cabrera <cpp.cabrera@gmail.com>
2012-02-27 13:28:06 -0500

    Update: implementation advances.
    - Matrix interface complete.
    - Started serial_matrix_implementation interface.

--- include/numerical/matrix/serial_matrix_implementation.hpp
+++ include/numerical/matrix/serial_matrix_implementation.hpp
@@ -2,15 +2,17 @@
 #define NUMERICAL_DETAIL_SERIAL_MATRIX_IMPLEMENTATION_HPP
 
 namespace numerical {
-	namespace detail {
+	namespace impl {
 		
 		template <class T,
 							class Allocator = std::allocator<T> >
 		class SerialMatrixImplementation {
-			
+
+    public:
+      
 			typedef T value_type;
 			typedef Allocator allocator_type;
-
+      
 		public:
 
 			explicit SerialMatrixImplementation(const size_t n);
