22c2be9bff4ea8724354c85c3ef03b207a99e3d2
Alejandro Cabrera <cpp.cabrera@gmail.com>
2012-03-23 09:43:59 -0400

    License: added license to Fast Slater components

--- include/fast_slater/implementation/slater_determinant/sherman_morrison_slater.cpp
+++ include/fast_slater/implementation/slater_determinant/sherman_morrison_slater.cpp
@@ -1,5 +1,6 @@
  /*
     Fast Slater - algorithms for the computation of Slater determinants.
+    Copyright (C) Alejandro Cabrera 2012
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
